9a7dd50e4a0ebe40bb1efb9830fcbd03d9238686
julian <julianespiloy08@gmail.com>
2023-12-03 02:49:38 +0800

    Initial commit

---
+++ components/Feature.js
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import React, { useMemo } from "react";
+import { motion } from "framer-motion";
+import getScrollAnimation from "../utils/getScrollAnimation";
+import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
+
+const features = [
+  "Provide a clear and concise summary of the account balance on the homepage or account dashboard.",
+  "Integrate tools that allow users to set and customize budgets based on different spending categories.",
+  "Showcase a detailed and secure transaction history that includes filters, search options, and categorization.",
+  "Highlight any automated savings features your bank offers, such as round-up transactions, recurring transfers to savings accounts, or goal-based savings.",
+];
+
+const Feature = () => {
+  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
+
+  return (
+    <div
+      className="max-w-screen-xl mt-8 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+      id="feature"
+    >
+      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
+        <ScrollAnimationWrapper className="flex w-full justify-end">
+          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
+            <Image
+              src="/assets/Mobile_Banking_3D_Illustration-removebg-preview.png"
+              alt="VPN Illustrasi"
+              layout="responsive"
+              quality={100}
+              height={414}
+              width={508}
+            />
+          </motion.div>
+        </ScrollAnimationWrapper>
+        <ScrollAnimationWrapper>
+          <motion.div
+            className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12"
+            variants={scrollAnimation}
+          >
+            <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
+              We Provide Many Features
+            </h3>
+            <p className="my-2 text-black-500">
+              TrustWorth Bank operates in the digital realm, providing a
+              seamless and private network for your banking needs. Our
+              commitment to high security ensures that your financial
+              transactions and information are safeguarded at every step.
+            </p>
+            <ul className="text-black-500 self-start list-inside ml-8">
+              {features.map((feature, index) => (
+                <motion.li
+                  className="relative circle-check custom-list"
+                  custom={{ duration: 2 + index }}
+                  variants={scrollAnimation}
+                  key={feature}
+                  whileHover={{
+                    scale: 1.1,
+                    transition: {
+                      duration: 0.2,
+                    },
+                  }}
+                >
+                  {feature}
+                </motion.li>
+              ))}
+            </ul>
+          </motion.div>
+        </ScrollAnimationWrapper>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
