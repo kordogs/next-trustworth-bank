d7aefe0fadca7b133b91d70c124b5ff4fc1ba9c6
julian <julianespiloy08@gmail.com>
2023-12-03 03:12:35 +0800

    Update SeoHead.js

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -8,13 +8,13 @@ const defaultMeta = {
   description:
     "Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest",
   // change base url of your web (without '/' at the end)
-  url: "https://next-landing-vpn.vercel.app",
   type: "website",
   robots: "follow, index",
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status
-  image: "https://next-landing-vpn.vercel.app/assets/card-image.png",
-  author: "Lorem Ipsum",
+  image:
+    "https://next-trustworth-bank.vercel.app//assets/People_pay_bills_mobile_banking-removebg-preview.png",
+  author: "Julian Espiloy",
 };
 
 /**
